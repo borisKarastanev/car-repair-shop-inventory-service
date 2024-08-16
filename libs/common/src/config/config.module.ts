b9108274d353892a92769ef0a1b5c52a29112fc8
BORIS KARASTANEV <b.karastanev@gmail.com>
2024-08-16 15:44:32 +0300

    Database and Config Setup

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import {
+  ConfigModule as NestConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+import { configValidationSchema } from 'config.schema';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      envFilePath: ['.env'],
+      validationSchema: configValidationSchema
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
