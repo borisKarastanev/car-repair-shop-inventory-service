894635d6f1bee12c47ef431ca829d07007eebc46
BORIS KARASTANEV <b.karastanev@gmail.com>
2024-08-23 15:06:17 +0300

    Added a Docker configuration and Enabled Logger

--- apps/cars/src/main.ts
+++ apps/cars/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { CarsModule } from './cars.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(CarsModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
